86bfbcc4ffb847c992dd684ea20d6dba8a4e2b7f
Chad Dyck <chad@chaddyck.com>
2019-10-02 17:26:08 -0600

    Added array of data to pull information from.

---
+++ data.js
@@ -0,0 +1,267 @@
+const mtxData = [
+	{
+		name: "Ringmaster Wings",
+		value: "320",
+		image: "https://web.poecdn.com/public/news/2019-09-08/Items/RingmasterWings.png",
+		rarity: "rare"
+	},
+
+	{
+		name: "Harlequin Wings",
+		value: "320",
+		image: "https://web.poecdn.com/public/news/2019-09-08/Items/HarlequinWings.png",
+		rarity: "rare"
+	},
+
+	{
+		name: "Ringmaster Body Armour",
+		value: "250",
+		image: "https://web.poecdn.com/public/news/2019-09-08/Items/RingmasterBody.png",
+		rarity: "rare"
+	},
+
+	{
+		name: "Harlequin Body Armour",
+		value: "250",
+		image: "https://web.poecdn.com/public/news/2019-09-08/Items/HarlequinBody.png",
+		rarity: "rare"
+	},
+
+	{
+		name: "Ringmaster Cloak",
+		value: "230",
+		image: "https://web.poecdn.com/public/news/2019-09-08/Items/RingmasterCloak.png",
+		rarity: "rare"
+	},
+
+	{
+		name: "Harlequin Cloak",
+		value: "230",
+		image: "https://web.poecdn.com/public/news/2019-09-08/Items/HarlequinCloak.png",
+		rarity: "rare"
+	},
+
+	{
+		name: "Ringmaster Character Effect",
+		value: "210",
+		image: "https://web.poecdn.com/public/news/2019-09-08/Items/RingmasterCharacterEffect.png",
+		rarity: "rare"
+	},
+
+	{
+		name: "Harlequin Character Effect",
+		value: "210",
+		image: "https://web.poecdn.com/public/news/2019-09-08/Items/HarlequinCharacterEffect.png",
+		rarity: "rare"
+	},
+
+	{
+		name: "Ringmaster Weapon Skin",
+		value: "180",
+		image: "https://web.poecdn.com/public/news/2019-09-08/Items/RingmasterWeaponSkin.png",
+		rarity: "rare"
+	},
+
+	{
+		name: "Harlequin Weapon Skin",
+		value: "180",
+		image: "https://web.poecdn.com/public/news/2019-09-08/Items/HarlequinWeaponSkin.png",
+		rarity: "rare"
+	},
+
+	{
+		name: "Ringmaster Weapon Effect",
+		value: "180",
+		image: "https://web.poecdn.com/public/news/2019-09-08/Items/RingmasterWeaponEffect.png",
+		rarity: "rare"
+	},
+
+	{
+		name: "Harlequin Weapon Effect",
+		value: "180",
+		image: "https://web.poecdn.com/public/news/2019-09-08/Items/HarleqinWeaponEffect.png",
+		rarity: "rare"
+	},
+
+	{
+		name: "Ringmaster Portal",
+		value: "150",
+		image: "https://web.poecdn.com/public/news/2019-09-08/Items/RingmasterPortalEffect.png",
+		rarity: "rare"
+	},
+
+	{
+		name: "Harlequin Portal",
+		value: "150",
+		image: "https://web.poecdn.com/public/news/2019-09-08/Items/HarlequinPortalEffect.png",
+		rarity: "rare"
+	},
+
+	{
+		name: "Ringmaster Jester Pet",
+		value: "160",
+		image: "https://web.poecdn.com/public/news/2019-09-08/Items/RingmasterCircusJesterPet.png",
+		rarity: "uncommon"
+	},
+
+	{
+		name: "Harlequin Monkey Pet",
+		value: "160",
+		image: "https://web.poecdn.com/public/news/2019-09-08/Items/CyrWheelMonkeyPet.png",
+		rarity: "uncommon"
+	},
+
+	{
+		name: "Ringmaster Helmet",
+		value: "130",
+		image: "https://web.poecdn.com/public/news/2019-09-08/Items/RingmasterHelmet.png",
+		rarity: "uncommon"
+	},
+
+	{
+		name: "Harlequin Helmet",
+		value: "130",
+		image: "https://web.poecdn.com/public/news/2019-09-08/Items/HarlequinHelmet.png",
+		rarity: "uncommon"
+	},
+
+	{
+		name: "Ringmaster Boots",
+		value: "80",
+		image: "https://web.poecdn.com/public/news/2019-09-08/Items/RingmasterBoots.png",
+		rarity: "uncommon"
+	},
+
+	{
+		name: "Harlequin Boots",
+		value: "80",
+		image: "https://web.poecdn.com/public/news/2019-09-08/Items/HarlequinBoots.png",
+		rarity: "uncommon"
+	},
+
+	{
+		name: "Ringmaster Gloves",
+		value: "80",
+		image: "https://web.poecdn.com/public/news/2019-09-08/Items/RingmasterGloves.png",
+		rarity: "uncommon"
+	},
+
+	{
+		name: "Harlequin Gloves",
+		value: "80",
+		image: "https://web.poecdn.com/public/news/2019-09-08/Items/HarlequinGloves.png",
+		rarity: "uncommon"
+	},
+
+	{
+		name: "Ringmaster Aura",
+		value: "125",
+		image: "https://web.poecdn.com/public/news/2019-09-08/Items/RingmasterAura.png",
+		rarity: "uncommon"
+	},
+
+	{
+		name: "Harlequin Aura",
+		value: "125",
+		image: "https://web.poecdn.com/public/news/2019-09-08/Items/HarlequinAura.png",
+		rarity: "uncommon"
+	},
+
+	{
+		name: "Ringmaster Whirling Blades",
+		value: "110",
+		image: "https://web.poecdn.com/public/news/2019-09-08/Items/RingmasterWhirlingBlades.png",
+		rarity: "uncommon"
+	},
+
+	{
+		name: "Harlequin Leap Slam",
+		value: "110",
+		image: "https://web.poecdn.com/public/news/2019-09-08/Items/HarlequinLeapSlam.png",
+		rarity: "uncommon"
+	},
+
+	{
+		name: "Ringmaster Hat",
+		value: "100",
+		image: "https://web.poecdn.com/public/news/2019-09-08/Items/RingmasterHat.png",
+		rarity: "common"
+	},
+
+	{
+		name: "Harlequin Hat",
+		value: "100",
+		image: "https://web.poecdn.com/public/news/2019-09-08/Items/HarlequinHat.png",
+		rarity: "common"
+	},
+
+	{
+		name: "Ringmaster Footprints",
+		value: "80",
+		image: "https://web.poecdn.com/public/news/2019-09-08/Items/RingmasterFootprints.png",
+		rarity: "common"
+	},
+
+	{
+		name: "Harlequin Footprints",
+		value: "80",
+		image: "https://web.poecdn.com/public/news/2019-09-08/Items/HarlequinFootprints.png",
+		rarity: "common"
+	},
+
+	{
+		name: "Ringmaster Building Supplies",
+		value: "60",
+		image: "https://web.poecdn.com/public/news/2019-09-08/Items/RingmasterBuilding.png",
+		rarity: "common"
+	},
+
+	{
+		name: "Harlequin Building Supplies",
+		value: "60",
+		image: "https://web.poecdn.com/public/news/2019-09-08/Items/HarlequinBuilding.png",
+		rarity: "common"
+	},
+
+	{
+		name: "Ringmaster Spectators",
+		value: "50",
+		image: "https://web.poecdn.com/public/news/2019-09-08/Items/RingmasterSpectators.png",
+		rarity: "common"
+	},
+
+	{
+		name: "Harlequin Spectators",
+		value: "50",
+		image: "https://web.poecdn.com/public/news/2019-09-08/Items/HarlequinSpectators.png",
+		rarity: "common"
+	},
+
+	{
+		name: "Ringmaster Fireworks Decoration",
+		value: "40",
+		image: "https://web.poecdn.com/public/news/2019-09-08/Items/RingmasterFireworksEnvironmentDecoration.png",
+		rarity: "common"
+	},
+
+	{
+		name: "Harlequin Apparition Decoration",
+		value: "40",
+		image: "https://web.poecdn.com/public/news/2019-09-08/Items/HarlequinApparitionDecoration.png",
+		rarity: "common"
+	},
+
+	{
+		name: "Ringmaster Parakeet Pet",
+		value: "30",
+		image: "https://web.poecdn.com/public/news/2019-09-08/Items/RingmasterParakeetPet.png",
+		rarity: "common"
+	},
+
+	{
+		name: "Harlequin Pranksters Pet",
+		value: "30",
+		image: "https://web.poecdn.com/public/news/2019-09-08/Items/JugglingPinsPet.png",
+		rarity: "common"
+	}
+]
