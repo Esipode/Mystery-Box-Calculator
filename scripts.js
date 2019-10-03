efd595a818e68715d2829a8b2a376df24588ee1d
Chad Dyck <chad@chaddyck.com>
2019-10-03 11:39:47 -0600

    Added more required variables, and created function to generate HTML elements from data.js.

--- scripts.js
+++ scripts.js
@@ -16,7 +16,31 @@ const mtxGlobals = {
 	//Percentage chance per item for each rarity
 	mtxPercentRare: (0.2 / (mtxData.filter((obj) => obj.rarity === 'rare').length)).toFixed(12),
 	mtxPercentUncommon: (0.35 / (mtxData.filter((obj) => obj.rarity === 'uncommon').length)).toFixed(12),
-	mtxPercentCommon: (0.45 / (mtxData.filter((obj) => obj.rarity === 'common').length)).toFixed(12)
+	mtxPercentCommon: (0.45 / (mtxData.filter((obj) => obj.rarity === 'common').length)).toFixed(12),
 
+	//Total point value of items selected from the box for each rarity
+	mtxTotalRareValue: '',
+	mtxTotalUncommonValue: '',
+	mtxTotalCommonValue: ''
 }
-console.log(mtxGlobals)
+
+//Creates UI elements for each item once document has loaded
+$(document).ready(function() {
+	for(let i = 0; i < mtxGlobals.mtxTotal; i++) {
+		jQuery('<label/>', {
+			"class": 'list_item transition' + ' ' + mtxData[i].rarity
+		}).appendTo('.container');
+		jQuery('<input/>', {
+			"class": "coins",
+			"type": "checkbox",
+			"value": mtxData[i].value
+		}).appendTo($('.list_item')[i]);
+		jQuery('<span/>', {
+			"class": "label-text",
+			"text":   mtxData[i].name
+		}).appendTo($('.coins')[i]);
+		jQuery('<img/>', {
+			"src": mtxData[i].image,
+		}).appendTo($('.coins')[i]);
+	}
+});
