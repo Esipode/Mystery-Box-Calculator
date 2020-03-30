e71298ef3ebaf44e87c80f576ebc1f9c76313f80
Chad Dyck <chad@chaddyck.com>
2020-03-29 23:31:20 -0600

    Added box selection feature.

--- src/box.js
+++ src/box.js
@@ -3,17 +3,22 @@ import React from 'react';
 export default class BoxSelection extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = {
-            
-        }
-        
+		this.state = {};
 	}
+	onChangeBox = () => {
+		if (this.props.activeBox !== this.props.name || this.props.activeBox === '') {
+			this.props.changeBox(this.props.name);
+		}
+	};
+	checkMatch = () => {
+		return this.props.activeBox === this.props.name ? ' selected' : ' unselected';
+	};
 	render() {
 		return (
-			<div className="box">
-               <img src={this.props.image} alt={this.props.name} />
-               <h3>{this.props.name}</h3>
+			<div className={`box ${this.checkMatch()}`} onClick={this.onChangeBox}>
+				<img src={this.props.image} alt={this.props.name} draggable="false" />
+				<h3>{this.props.name}</h3>
 			</div>
-		)
+		);
 	}
 }
--- src/boxSelection.js
+++ src/boxSelection.js
@@ -1,25 +1,34 @@
 import React from 'react';
-import {images as box} from './data.json';
-import Box from './box.js'
+import { images as box } from './data.json';
+import Box from './box';
 
 export default class BoxSelection extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-            boxList: box
-        }
-        console.log(this.state.boxList);
+			boxList: box,
+			activeBox: ''
+		};
 	}
+	onChangeActiveBox = (newBox) => {
+		this.setState({
+			activeBox: newBox
+		});
+		this.props.changeMTX(newBox);
+	};
 	render() {
 		return (
 			<div className="boxSelection">
-                {this.state.boxList.map((image, index) => (
-                    <Box
-                        name={this.state.boxList[index].name}
-                        image={this.state.boxList[index].image}
-                    />
+				{this.state.boxList.map((image, index) => (
+					<Box
+						name={this.state.boxList[index].name}
+						image={this.state.boxList[index].image}
+						key={this.state.boxList[index].name}
+						changeBox={this.onChangeActiveBox}
+						activeBox={this.state.activeBox}
+					/>
 				))}
 			</div>
-		)
+		);
 	}
 }
