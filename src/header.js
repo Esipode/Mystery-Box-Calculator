dab2b30446f5b255c2b88046022f44ea93269edb
Chad Dyck <chad@chaddyck.com>
2020-03-31 15:11:42 -0600

    Simplified function.

--- src/header.js
+++ src/header.js
@@ -11,19 +11,15 @@ export default class Header extends React.Component {
 								]
 		};
 	}
-	onStepBack = () => {
-		this.props.changeStep(this.props.curStep - 1)
-	}
-	onStepForward = () => {
-		this.props.changeStep(this.props.curStep + 1)
+	onStepButton = (direction) => {
+		this.props.changeStep(this.props.curStep + direction)
 	}
 	onCheckStepVisibility = (step) => {
 		switch (step) {
 			case 0:
 				return (this.props.boxSelected === "") ? {display: 'none'} : {display: 'flex'};
 			case 1:
-				console.log(this.props.activeMTX);
-				return (this.props.activeMTX === []) ? {display: 'none'} : {display: 'flex'};
+				return (this.props.activeMTX.length < 1) ? {display: 'none'} : {display: 'flex'};
 			case 2:
 				return {
 					display: 'none'
@@ -39,7 +35,7 @@ export default class Header extends React.Component {
 	render() {
 		return (
 			<div className="header">
-				<div className="backStep" onClick={this.onStepBack} style={{display: this.props.curStep > 0 ? 'flex' : 'none'}}>
+				<div className="backStep" onClick={() => this.onStepButton(-1)} style={{display: this.props.curStep > 0 ? 'flex' : 'none'}}>
 					<div>
 						<h3>Step {this.props.curStep}</h3>
 						<span>{this.state.stepInfo[this.props.curStep - 1]}</span>
@@ -49,7 +45,7 @@ export default class Header extends React.Component {
 				<LogoSVG />
 				<h1>MTX Calc</h1>
 				<h2>Step {this.props.curStep + 1}:<span>{this.state.stepInfo[this.props.curStep]}</span></h2>
-				<div className="forwardStep" onClick={this.onStepForward} style={this.onCheckStepVisibility(this.props.curStep)}>
+				<div className="forwardStep" onClick={() => this.onStepButton(+1)} style={this.onCheckStepVisibility(this.props.curStep)}>
 					<i className="fas fa-arrow-circle-right" />
 					<div>
 						<h3>Step {this.props.curStep + 2}</h3>
