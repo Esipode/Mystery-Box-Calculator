25b3f29d2f2e1de8a963eb0d900222fda903e616
Chad Dyck <chad@chaddyck.com>
2020-03-31 20:43:38 -0600

    Added start/stop button to box simulator UI.

--- src/boxSimulator.js
+++ src/boxSimulator.js
@@ -3,7 +3,8 @@ export default class BoxSimulator extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			boxVal: ''
+			boxVal: '',
+			running: false
 		};
 	}
 	render() {
@@ -11,9 +12,10 @@ export default class BoxSimulator extends React.Component {
 			<div className="boxSimulator">
 				<div className="searchContainer">
 					<h4>Boxes:
-					<input type="text" pattern={"[0-9]*"} maxlength="3" onChange={(e) => this.setState({boxVal: e.target.value.replace(/\D/,'')})} value={this.state.boxVal} placeholder="# of boxes" />
+					<input type="text" pattern={"[0-9]*"} maxLength="3" onChange={(e) => this.setState({boxVal: e.target.value.replace(/\D/,'')})} value={this.state.boxVal} placeholder="# of boxes" />
 					</h4>
 					<h4>Points: {this.state.boxVal * 30}</h4>
+					<button onClick={() => {this.setState({running: this.state.running ? false : true})}} style={{backgroundColor: this.state.running ? '#ff5f5f' : 'var(--selectColor)'}}>{this.state.running ? 'Stop' : 'Start'}</button>
 				</div>
 				{/* <table>
 					<thead>
