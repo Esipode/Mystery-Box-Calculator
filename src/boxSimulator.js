327fccf64a8ac1dfc931507a7b84ef82689d4e87
Chad Dyck <chad@chaddyck.com>
2020-04-01 12:37:47 -0600

    Added more start/stop functionality to box simulator.

--- src/boxSimulator.js
+++ src/boxSimulator.js
@@ -12,10 +12,21 @@ export default class BoxSimulator extends React.Component {
 			<div className="boxSimulator">
 				<div className="searchContainer">
 					<h4>Boxes:
-					<input type="text" pattern={"[0-9]*"} maxLength="3" onChange={(e) => this.setState({boxVal: e.target.value.replace(/\D/,'')})} value={this.state.boxVal} placeholder="# of boxes" />
+					<input 
+						type="text" 
+						pattern={"[0-9]*"} 
+						maxLength="3" 
+						disabled={this.state.running} 
+						onChange={(e) => this.setState({boxVal: e.target.value.replace(/\D/,'')})} 
+						value={this.state.boxVal} 
+						placeholder="# of boxes" 
+					/>
 					</h4>
 					<h4>Points: {this.state.boxVal * 30}</h4>
-					<button onClick={() => {this.setState({running: this.state.running ? false : true})}} style={{backgroundColor: this.state.running ? '#ff5f5f' : 'var(--selectColor)'}}>{this.state.running ? 'Stop' : 'Start'}</button>
+					<button 
+						onClick={() => {this.setState({running: this.state.running ? false : true})}} 
+						style={{borderColor: this.state.running ? '#ff5f5f' : 'var(--selectColor)'}}><i className={this.state.running ? 'fas fa-stop' : 'fas fa-play' }/>
+					</button>
 				</div>
 				{/* <table>
 					<thead>
