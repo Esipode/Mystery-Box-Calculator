0038bcca0417883366b8b3f60fe297955c5cbbe4
Chad Dyck <chad@chaddyck.com>
2020-04-16 15:54:48 -0600

    Statistics now show wanted/unwanted ratio more colorfully.

--- src/stat.js
+++ src/stat.js
@@ -12,7 +12,7 @@ export default class Stat extends React.Component {
 			<tr className={`stat ${this.props.stat.rarity}`}>
 				<td>{this.props.stat.name}</td>
 				<td>{this.props.stat.count}</td>
-				<td>{this.props.stat.wanted} / {this.props.stat.unwanted}</td>
+				<td><span className="wanted">{((this.props.stat.wanted / this.props.stat.count) * 100).toFixed(0)}%</span> / <span className="unwanted">{((this.props.stat.unwanted / this.props.stat.count) * 100).toFixed(0)}%</span></td>
 			</tr>
 		);
 	}
