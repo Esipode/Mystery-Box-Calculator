1d826e001131fdc9287fd2cdf818a5ccd73e052b
Chad Dyck <chad@chaddyck.com>
2020-04-22 14:05:33 -0600

    Fixed 'null' percentages when amount was 0 or undefined.

--- src/stat.js
+++ src/stat.js
@@ -12,7 +12,7 @@ export default class Stat extends React.Component {
 			<tr className={`stat ${this.props.stat.rarity}`}>
 				<td>{this.props.stat.name}</td>
 				<td>{this.props.stat.count}</td>
-				<td><span className="wanted">{((this.props.stat.wanted / this.props.stat.count) * 100).toFixed(0)}%</span> / <span className="unwanted">{((this.props.stat.unwanted / this.props.stat.count) * 100).toFixed(0)}%</span></td>
+				<td><span className="wanted">{((this.props.stat.wanted ? this.props.stat.wanted : 0 / this.props.stat.count ? this.props.stat.count : 0) * 100).toFixed(0)}%</span> / <span className="unwanted">{((this.props.stat.unwanted ? this.props.stat.unwanted : 0 / this.props.stat.count ? this.props.stat.count : 0) * 100).toFixed(0)}%</span></td>
 			</tr>
 		);
 	}
