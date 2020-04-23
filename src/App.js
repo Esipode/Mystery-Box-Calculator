095fab32433c29f5a5bde1d93e245e1e87529791
Chad Dyck <chad@chaddyck.com>
2020-04-23 00:18:58 -0600

    Unselected/non-selected items will now have the selected property set to false. This was causing an issue with statistics tracking.

--- src/App.js
+++ src/App.js
@@ -56,6 +56,7 @@ export default class App extends React.Component {
 		}
 		else {
 			arr = arr.filter((item) => item !== mtx);
+			mtx.selected = false;
 		}
 		this.toggleStepThree(true);
 		this.setState({
