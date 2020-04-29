8d8202de4d3ad0ed1dec9a8235a97af4866ac291
Chad Dyck <chad@chaddyck.com>
2020-04-29 11:15:47 -0600

    Fixed statistics ratio showing NaN values.

--- src/stat.js
+++ src/stat.js
@@ -7,12 +7,24 @@ export default class Stat extends React.Component {
 			
 		};
 	}
+	calcPercentage = (total) => {
+		let percentage = ((total / this.props.stat.count) * 100).toFixed(0);
+		return isNaN(percentage) ? 0 : percentage;
+	}
 	render() {
 		return (
 			<tr className={`stat ${this.props.stat.rarity}`}>
 				<td>{this.props.stat.name}</td>
 				<td>{this.props.stat.count}</td>
-				<td><span className="wanted">{((this.props.stat.wanted / this.props.stat.count) * 100).toFixed(0)}%</span> / <span className="unwanted">{((this.props.stat.unwanted / this.props.stat.count) * 100).toFixed(0)}%</span></td>
+				<td>
+					<span className="wanted">
+						{this.calcPercentage(this.props.stat.wanted)}%
+					</span>
+					/ 
+					<span className="unwanted">
+						{this.calcPercentage(this.props.stat.unwanted)}%
+					</span>
+				</td>
 			</tr>
 		);
 	}
