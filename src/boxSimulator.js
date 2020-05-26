112a27ad94b857f9811b918f0eb5547e9c159c36
Chad Dyck <chad@chaddyck.com>
2020-05-26 16:58:27 -0600

    Changed server post request to production URL.

--- src/boxSimulator.js
+++ src/boxSimulator.js
@@ -155,7 +155,7 @@ export default class BoxSimulator extends React.Component {
 			resultsPending: true
 		})
 		let stat = await this.listStats();
-		axios.post('http://localhost:5000/stats/update/'+stat._id, stat)
+		axios.post('/stats/update/'+stat._id, stat)
 			.then(res => {
 				console.log('Stats updated!');
 				this.props.simList(res.data);
