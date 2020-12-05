88b860a84061898c30910dd4fe39d5b5efa98632
Chad Dyck <chad@chaddyck.com>
2020-12-05 16:24:07 -0700

    Switched from CSS to SCSS.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import Header from './header';
 import BoxSelection from './boxSelection';
 import MTXSelection from './mtxSelection';
